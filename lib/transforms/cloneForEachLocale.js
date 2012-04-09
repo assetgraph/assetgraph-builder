assetgraph/assetgraph-builder

commit e4b4cdcd7f55c6063bbe0998a54876c9ab8be203

buildProduction: Remove JavaScriptAmd(Define|Require) relations immediately after running transforms.cloneForEachLocale (revisit when adding support for lazy loading).

bin/buildProduction
@@ -72,6 +72,7 @@ new AssetGraph({root: commandLineOptions.root})
     .endif()
     .if(localeIds)
         .cloneForEachLocale({type: 'Html', isInitial: true}, localeIds)
+        .removeRelations({type: ['JavaScriptAmdDefine', 'JavaScriptAmdRequire']})
         .runJavaScriptConditionalBlocks({isInitial: true}, 'localize', true)
     .endif()
     .removeAssets({type: 'I18n'}, true)

commit 5c3b57fdcb258b5ade9a0b38d2a58c35762cbe01

buildProduction/transforms.cloneForEachLocale: Fixed bug introduced in e4b4cdc.

bin/buildProduction
@@ -72,7 +72,6 @@ new AssetGraph({root: commandLineOptions.root})
     .endif()
     .if(localeIds)
         .cloneForEachLocale({type: 'Html', isInitial: true}, localeIds)
-        .removeRelations({type: ['JavaScriptAmdDefine', 'JavaScriptAmdRequire']})
         .runJavaScriptConditionalBlocks({isInitial: true}, 'localize', true)
     .endif()
     .removeAssets({type: 'I18n'}, true)

lib/transforms/cloneForEachLocale.js
@@ -5,7 +5,8 @@ var _ = require('underscore'),
 module.exports = function (queryObj, localeIds) {
     return function cloneForEachLocale(assetGraph) {
         assetGraph.findAssets(_.extend({type: 'Html'}, queryObj)).forEach(function (originalHtmlAsset) {
-            var nonInlineAssetsToCloneById = {};
+            var nonInlineJavaScriptsToCloneById = {},
+                nonInlineTemplatesToCloneById = {};
 
             // First note which JavaScript assets need to be cloned for each locale:
             assetGraph.findRelations({type: ['HtmlScript', 'HtmlRequireJsMain'], from: originalHtmlAsset, node: {id: query.not('oneBootstrapper')}, to: {isInline: false}}).forEach(function (htmlScript) {
@@ -14,7 +15,7 @@ module.exports = function (queryObj, localeIds) {
                 // Check if the JavaScript asset has relations to a template that need localization:
                 assetGraph.findRelations({from: htmlScript.to, to: {isInline: false, type: 'KnockoutJsTemplate'}}).forEach(function (templateRelation) {
                     i18nTools.eachI18nTagInHtmlDocument(templateRelation.to.parseTree, function () {
-                        nonInlineAssetsToCloneById[templateRelation.to.id] = templateRelation.to;
+                        nonInlineTemplatesToCloneById[templateRelation.to.id] = templateRelation.to;
                         doClone = true;
                     });
                 });
@@ -27,7 +28,7 @@ module.exports = function (queryObj, localeIds) {
                     });
                 }
                 if (doClone) {
-                    nonInlineAssetsToCloneById[htmlScript.to.id] = htmlScript.to;
+                    nonInlineJavaScriptsToCloneById[htmlScript.to.id] = htmlScript.to;
                 }
             });
             localeIds.forEach(function (localeId) {
@@ -43,14 +44,20 @@ module.exports = function (queryObj, localeIds) {
                 localizedHtml.markDirty();
 
                 assetGraph.findRelations({type: ['HtmlScript', 'HtmlRequireJsMain'], from: localizedHtml, node: {id: query.not('oneBootstrapper')}}).forEach(function (htmlScript) {
-                    if (htmlScript.to.id in nonInlineAssetsToCloneById) {
+                    if (htmlScript.to.id in nonInlineJavaScriptsToCloneById) {
+                        var hasAmdRelations = assetGraph.findRelations({from: htmlScript.to, type: ['JavaScriptAmdDefine', 'JavaScriptAmdRequire']}).length > 0;
                         htmlScript.to.clone(htmlScript);
+                        if (!hasAmdRelations) {
+                            assetGraph.findRelations({from: htmlScript.to, type: ['JavaScriptAmdDefine', 'JavaScriptAmdRequire']}).forEach(function (relation) {
+                                assetGraph.removeRelation(relation);
+                            });
+                        }
                     }
                     i18nTools.eachOneTrInAst(htmlScript.to.parseTree,
                                              i18nTools.createOneTrReplacer(allReachableKeysForLocale, localeId));
 
                     assetGraph.findRelations({from: htmlScript.to, type: 'JavaScriptOneGetText', to: {isInline: false, type: 'KnockoutJsTemplate'}}).forEach(function (templateRelation) {
-                        if (templateRelation.to.id in nonInlineAssetsToCloneById) {
+                        if (templateRelation.to.id in nonInlineTemplatesToCloneById) {
                             templateRelation.to.clone(templateRelation);
                             i18nTools.eachI18nTagInHtmlDocument(templateRelation.to.parseTree,
                                                                 i18nTools.createI18nTagReplacer(allReachableKeysForLocale, localeId));
@@ -71,7 +78,7 @@ module.exports = function (queryObj, localeIds) {
             assetGraph.removeAsset(originalHtmlAsset);
 
             // Remove orphaned JavaScript and templates:
-            _.values(nonInlineAssetsToCloneById).forEach(function (asset) {
+            _.values(nonInlineJavaScriptsToCloneById).concat(_.values(nonInlineTemplatesToCloneById)).forEach(function (asset) {
                 if (assetGraph.findRelations({to: asset}).length === 0) {
                     assetGraph.removeAsset(asset);
                 }