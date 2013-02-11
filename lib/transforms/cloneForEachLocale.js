assetgraph/assetgraph-builder

commit a554609185dbe1d7c5ed2f3ab3bb284fa0673e41

cloneForEachLocale: Added poor man's optimization of if statements that helps out with huge inter builds.

lib/transforms/cloneForEachLocale.js
@@ -109,21 +109,49 @@ module.exports = function (queryObj, options) {
                             return;
                         }
                         i18nTools.eachTrInAst(asset.parseTree, trReplacer);
-                        var q = [asset.parseTree];
-                        while (q.length) {
-                            var node = q.pop();
 
-                            if ((node[0] === 'name' && Object.prototype.hasOwnProperty.call(globalValueByName, node[1]))) {
-                                Array.prototype.splice.apply(node, [0, node.length].concat(uglifyAst.objToAst(globalValueByName[node[1]])));
-                            } else {
-                                // Don't dive into the LVALUE in assignments:
-                                for (var i = (node[0] === 'assign' ? 3 : 0) ; i < node.length ; i += 1) {
-                                    if (Array.isArray(node[i])) {
-                                        q.push(node[i]);
+                        var ifAndParentNodesToOptimize = [],
+                            walker = uglifyJs.uglify.ast_walker();
+                        walker.with_walkers({
+                            name: function (name) {
+                                if (Object.prototype.hasOwnProperty.call(globalValueByName, name)) {
+                                    var stack = walker.stack(),
+                                        node = stack[stack.length - 1];
+                                    if (stack[stack.length - 2][0] !== 'assign') {
+                                        Array.prototype.splice.apply(node, [0, node.length].concat(uglifyAst.objToAst(globalValueByName[node[1]])));
+                                        for (var i = stack.length - 2 ; i >= 0 ; i -= 1) {
+                                            var parentNode = stack[i];
+                                            if (parentNode[0] === 'if') {
+                                                ifAndParentNodesToOptimize.push([parentNode, stack[i - 1]]);
+                                            } else if (parentNode[0] !== 'binary' && parentNode[0] !== 'unary-prefix' && parentNode[0] !== 'unary-postfix') {
+                                                break;
+                                            }
+                                        }
                                     }
                                 }
                             }
-                        }
+                        }, function () {
+                            walker.walk(asset.parseTree);
+                        });
+                        // Poor man's optimization: Replace if(false) with the else part and if(true) with the then part.
+                        ifAndParentNodesToOptimize.forEach(function (ifAndParentNode) {
+                            var ifNode = ifAndParentNode[0],
+                                parentNode = ifAndParentNode[1],
+                                statementArray = parentNode[0] === 'toplevel' && parentNode[1];
+                            if (statementArray) {
+                                var indexInStatementArray = statementArray.indexOf(ifNode);
+                                if (indexInStatementArray !== -1) {
+                                    var foldedAst = uglifyAst.foldConstant(ifNode[1]);
+                                    if (foldedAst[0] === 'unary-prefix' && foldedAst[1] === '!' && foldedAst[2][0] === 'num') {
+                                        var statementAsts = foldedAst[2][1] ? (ifNode[3] ? [ifNode[3]] : []) : (ifNode[2] ? [ifNode[2]] : []);
+                                        if (statementAsts.length === 1 && statementAsts[0][0] === 'block') {
+                                            statementAsts = statementAsts[0][1];
+                                        }
+                                        Array.prototype.splice.apply(statementArray, [indexInStatementArray, 1].concat(statementAsts));
+                                    }
+                                }
+                            }
+                        });
                         asset.markDirty();
                     } else if (asset.isHtml) {
                         var document = asset.parseTree;

test/cloneForEachLocale-test.js
@@ -760,5 +760,20 @@ vows.describe('Make a clone of each Html file for each language').addBatch({
         'the Danish Html asset should have two outgoing HtmlImage relations': function (assetGraph) {
             assert.equal(assetGraph.findRelations({type: 'HtmlImage', from: {url: /\/index\.da\.html$/}}).length, 2);
         }
+    },
+    'After loading a test case with a script that uses LOCALEID in a conditional, then run the cloneForEachLocale transform': {
+        topic: function () {
+            new AssetGraph({root: __dirname + '/cloneForEachLocale/constantFoldLocaleId/'})
+                .loadAssets('index.html')
+                .populate()
+                .cloneForEachLocale({type: 'Html'}, {localeIds: ['en', 'da']})
+                .run(this.callback);
+        },
+        'the script in the English Html asset should have the expected contents': function (assetGraph) {
+            assert.equal(assetGraph.findRelations({from: {url: /\/index\.en\.html$/}})[0].to.text, 'alert("foo");alert("baz");alert("quux")');
+        },
+        'the script in the Danish Html asset should have the expected contents': function (assetGraph) {
+            assert.equal(assetGraph.findRelations({from: {url: /\/index\.da\.html$/}})[0].to.text, 'alert("foo");alert("bar");alert("boz");alert("quux")');
+        }
     }
 })['export'](module);

test/cloneForEachLocale/constantFoldLocaleId/index.html
@@ -1,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head></head>
+<body>
+<script>
+    alert("foo");
+    if (LOCALEID === 'da') {
+        alert("bar");
+    }
+    if (LOCALEID === 'en') {
+        alert("baz");
+    } else {
+        alert("boz");
+    }
+    alert("quux");
+</script>
+</body>
+</html>

commit 5ecdb9c356f5dd1f9d21a486f4604bfaf7349e36

cloneForEachLocale: Do dive into the RHS of assign nodes when replacing LOCALEID etc. Fixes bug introduced in a554609 (1.5.1+).

lib/transforms/cloneForEachLocale.js
@@ -116,8 +116,16 @@ module.exports = function (queryObj, options) {
                             name: function (name) {
                                 if (Object.prototype.hasOwnProperty.call(globalValueByName, name)) {
                                     var stack = walker.stack(),
-                                        node = stack[stack.length - 1];
-                                    if (stack[stack.length - 2][0] !== 'assign') {
+                                        node = stack[stack.length - 1],
+                                        stackIndex = stack.length - 2;
+
+                                    // Make sure that we're not in the LHS of an ['assign', ...] node:
+
+                                    while (stackIndex >= 0 && stack[stackIndex][0] === 'dot') {
+                                        stackIndex -= 1;
+                                    }
+
+                                    if (stack[stackIndex][0] !== 'assign' || stack[stackIndex][2] !== stack[stackIndex + 1]) {
                                         Array.prototype.splice.apply(node, [0, node.length].concat(uglifyAst.objToAst(globalValueByName[node[1]])));
                                         for (var i = stack.length - 2 ; i >= 0 ; i -= 1) {
                                             var parentNode = stack[i];