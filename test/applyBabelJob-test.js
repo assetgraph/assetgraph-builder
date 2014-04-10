9de0cd3ce9d6f01d3a5d1da42e6959ecde516b14
Andreas Lind Petersen <andreas@one.com>
2014-04-10 21:54:26 +0200

    applyBabelJob: Leave TR and data-i18n occurrences alone for keys that haven't changed in the default locale.
    Changed the test to use mocha/unexpected because vows screwed up the string diffs and didn't support object diffs.

--- test/applyBabelJob-test.js
+++ test/applyBabelJob-test.js
@@ -1,71 +1,88 @@
 var childProcess = require('child_process'),
     fs = require('fs'),
     Path = require('path'),
-    vows = require('vows'),
-    assert = require('assert'),
+    expect = require('unexpected'),
     temp = require('temp');
 
-vows.describe('applyBabelJob').addBatch({
-    'After running applyBabelJob on a test case': {
-        topic: function (babelDir) {
-            var cb = this.callback,
-                babelDir = Path.resolve(__dirname, 'applyBabelJob/translationjob'),
-                tmpTestCaseCopyDir = temp.mkdirSync(),
-                copyCommand = "cp '" + __dirname + "/applyBabelJob'/index.* " + tmpTestCaseCopyDir;
-            childProcess.exec(copyCommand, function (err, stdout, stderr) {
-                if (err) {
-                    return cb(new Error(copyCommand + " failed: STDERR:" + stderr + "\nSTDOUT:" + stdout));
+describe('applyBabelJob', function () {
+    it('should handle a complex test case', function (done) {
+        var babelDir = Path.resolve(__dirname, 'applyBabelJob/translationjob'),
+            tmpTestCaseCopyDir = temp.mkdirSync(),
+            copyCommand = "cp '" + __dirname + "/applyBabelJob'/index.* " + tmpTestCaseCopyDir;
+        childProcess.exec(copyCommand, function (err, stdout, stderr) {
+            if (err) {
+                return done(new Error(copyCommand + " failed: STDERR:" + stderr + "\nSTDOUT:" + stdout));
+            }
+            var applyBabelJobProcess = childProcess.spawn(__dirname + '/../bin/applyBabelJob', [
+                '--babeldir', babelDir,
+                '--root', tmpTestCaseCopyDir,
+                '--defaultlocale', 'en',
+                '--locales', 'en,da',
+                '--i18n', tmpTestCaseCopyDir + '/index.i18n',
+                '--replace',
+                tmpTestCaseCopyDir + '/index.html'
+            ]);
+applyBabelJobProcess.stdout.pipe(process.stdout);
+applyBabelJobProcess.stderr.pipe(process.stdout);
+            applyBabelJobProcess.on('exit', function (exitCode) {
+                if (exitCode) {
+                    done(new Error("The applyBabelJob process ended with a non-zero exit code: " + exitCode));
+                } else {
+                    expect(JSON.parse(fs.readFileSync(tmpTestCaseCopyDir + '/index.i18n')), 'to equal', {
+                        ComplexKey: {
+                            da: {
+                                that: 'også kommer',
+                                back: 'det samme'
+                            },
+                            en: {
+                                that: 'also comes',
+                                back: 'the same'
+                            }
+                        },
+                        WeirdlyFormattedKey: {
+                            da: 'der kommer uændret retur i oversættelsesjobbet',
+                            en: 'that comes back the same in the translation job'
+                        },
+                        bar: {
+                            da: 'BarOversat',
+                            en: 'BarProofRead'
+                        },
+                        foo: {
+                            da: 'FooOversat',
+                            en: 'FooProofRead'
+                        },
+                        placeholders: {
+                            da: 'Denne oversatte nøgle har {0} pladsholdere',
+                            en: 'This proofread key has {0} proofread placeholders'
+                        }
+                    });
+                    expect(fs.readFileSync(tmpTestCaseCopyDir + '/index.html', 'utf-8'), 'to equal',
+                        '<!DOCTYPE html>\n' +
+                        '<html>\n' +
+                        '    <head>\n' +
+                        '        <title data-i18n="foo">FooProofRead</title>\n' +
+                        '    </head>\n' +
+                        '    <body>\n' +
+                        '        <script>\n' +
+                        '            alert(TR(\'bar\', \'BarProofRead\'));\n' +
+                        '            alert(TR(\'WeirdlyFormattedKey\', \'that \' + \'comes \' + \'back the same in the translation job\'));\n' +
+                        '            alert(TR(\'ComplexKey\', {\n' +
+                        '                that: \'also comes\',\n' +
+                        '                back: \'the same\'\n' +
+                        '            }));\n' +
+                        '        </script>\n' +
+                        '        <span data-i18n="placeholders">This proofread key has <span>some</span> proofread placeholders</span>\n' +
+                        '        <span data-bind="text: foo < 4 && bar > 2"></span>\n' +
+                        '        <span data-i18n="WeirdlyFormattedKey">\n' +
+                        '            that comes back the same\n' +
+                        '            in the translation job\n' +
+                        '        </span>\n' +
+                        '    </body>\n' +
+                        '</html>\n'
+                    );
+                    done();
                 }
-                var applyBabelJobProcess = childProcess.spawn(__dirname + '/../bin/applyBabelJob', [
-                    '--babeldir', babelDir,
-                    '--root', tmpTestCaseCopyDir,
-                    '--defaultlocale', 'en',
-                    '--locales', 'en,da',
-                    '--i18n', tmpTestCaseCopyDir + '/index.i18n',
-                    '--replace',
-                    tmpTestCaseCopyDir + '/index.html'
-                ]);
-                applyBabelJobProcess.on('exit', function (exitCode) {
-                    if (exitCode) {
-                        cb(new Error("The applyBabelJob process ended with a non-zero exit code: " + exitCode));
-                    } else {
-                        cb(null, tmpTestCaseCopyDir);
-                    }
-                });
             });
-        },
-        'index.i18n should be updated with the translations': function (tmpTestCaseCopyDir) {
-            assert.deepEqual(JSON.parse(fs.readFileSync(tmpTestCaseCopyDir + '/index.i18n')), {
-                bar: {
-                    da: 'BarOversat',
-                    en: 'BarProofRead'
-                },
-                foo: {
-                    da: 'FooOversat',
-                    en: 'FooProofRead'
-                },
-                placeholders: {
-                    da: 'Denne oversatte nøgle har {0} pladsholdere',
-                    en: 'This proofread key has {0} proofread placeholders'
-                }
-            });
-        },
-        'index.html should be updated with the proofread English keys': function (tmpTestCaseCopyDir) {
-            assert.equal(fs.readFileSync(tmpTestCaseCopyDir + '/index.html', 'utf-8'),
-                '<!DOCTYPE html>\n' +
-                '<html>\n' +
-                '    <head>\n' +
-                '        <title data-i18n="foo">FooProofRead</title>\n' +
-                '    </head>\n' +
-                '    <body>\n' +
-                '        <script>\n' +
-                '            alert(TR(\'bar\', \'BarProofRead\'));\n' +
-                '        </script>\n' +
-                '        <span data-i18n="placeholders">This proofread key has <span>some</span> proofread placeholders</span>\n' +
-                '        <span data-bind="text: foo < 4 && bar > 2"></span>\n' +
-                '    </body>\n' +
-                '</html>\n'
-            );
-        }
-   }
-})['export'](module);
+        });
+    });
+});
