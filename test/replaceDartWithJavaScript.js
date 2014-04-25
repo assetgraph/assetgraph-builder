c6e3b0914cb97ec3d19f688b4d3bf3019b9e52a6
Andreas Lind Petersen <andreas@one.com>
2014-04-25 23:24:18 +0200

    Converted replaceDartWithJavaScript test to mocha/unexpected.

---
+++ test/replaceDartWithJavaScript.js
@@ -0,0 +1,20 @@
+var expect = require('./unexpected-with-plugins'),
+    AssetGraph = require('../lib/AssetGraph');
+
+describe('replaceDartWithJavaScript', function (done) {
+    it('should handle a test case with 2 Dart assets', function (done) {
+        new AssetGraph({root: __dirname + '/replaceDartWithJavaScript/'})
+            .loadAssets('index.html')
+            .populate()
+            .queue(function (assetGraph) {
+                expect(assetGraph, 'to contain relation', 'HtmlDart');
+                expect(assetGraph, 'to contain no relations', 'HtmlScript');
+            })
+            .replaceDartWithJavaScript()
+            .queue(function (assetGraph) {
+                expect(assetGraph, 'to contain no relations', 'HtmlDart');
+                expect(assetGraph, 'to contain relation', 'HtmlScript');
+            })
+            .run(done);
+    });
+});
