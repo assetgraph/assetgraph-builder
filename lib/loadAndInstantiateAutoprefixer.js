assetgraph/assetgraph-builder

commit 7ba12f5395d05ba0283e62b23eab7ba05082611e

Centralize instantiation of autoprefixer, and avoid deprecation warning with autoprefixer 3+.

lib/loadAndInstantiateAutoprefixer.js
@@ -1,0 +1,24 @@
+var semver = require('semver');
+
+module.exports = function loadAndInstantiateAutoprefixer(browsers, errorMessageIfUnavailable) {
+    if (browsers && !Array.isArray(browsers)) {
+        browsers = String(browsers).split(/,\s*/);
+    }
+
+    var autoprefixer;
+    try {
+        autoprefixer = require('autoprefixer');
+    } catch (e) {
+        if (errorMessageIfUnavailable) {
+            e.message = errorMessageIfUnavailable + '\n' + e.message;
+            throw err;
+        }
+    }
+
+    if (semver.satisfies(require('autoprefixer/package.json').version, '>= 3.0.0')) {
+        autoprefixer = autoprefixer(browsers ? {browsers: browsers} : {});
+    } else {
+        autoprefixer = autoprefixer(browsers);
+    }
+    return autoprefixer;
+};

lib/transforms/autoprefixer.js
@@ -4,24 +4,16 @@
  * - Runs autoprefixer with the supplied options on each css asset
  */
 
+var loadAndInstantiateAutoprefixer = require('../loadAndInstantiateAutoprefixer');
+
 module.exports = function (options) {
     return function autoprefixer(assetGraph) {
         var cssAssets = assetGraph.findAssets({type: 'Css'});
 
         if (cssAssets.length > 0) {
             // See https://github.com/ai/autoprefixer#browsers
-            var autoprefix;
-            if (typeof options === 'function') {
-                autoprefix = options;
-            } else {
-                var browsers = typeof options === 'string' ? options.split(/,\s*/) : options;
-                try {
-                    autoprefix = require('autoprefixer')(browsers);
-                } catch (e) {
-                    assetGraph.emit('warn', new Error('autoprefixer transform: Found ' + cssAssets.length + ' css asset(s), but no autoprefixer module is available. Please use npm to install autoprefixer in your project so the autoprefixer transform can require it.'));
-                    return;
-                }
-            }
+            var browsers = typeof options === 'string' ? options.split(/,\s*/) : options,
+                autoprefix = loadAndInstantiateAutoprefixer(browsers, 'autoprefixer transform: Found ' + cssAssets.length + ' css asset(s), but no autoprefixer module is available. Please use npm to install autoprefixer in your project so the autoprefixer transform can require it.');
 
             cssAssets.forEach(function (cssAsset) {
                 cssAsset.text = autoprefix.process(cssAsset.text).css;

lib/transforms/buildProduction.js
@@ -1,4 +1,5 @@
-var urlTools = require('urltools');
+var urlTools = require('urltools'),
+    loadAndInstantiateAutoprefixer = require('../loadAndInstantiateAutoprefixer');
 
 module.exports = function (options) {
     options = options || {};
@@ -11,11 +12,8 @@ module.exports = function (options) {
         };
 
     if (options.browsers) {
-        try {
-            autoprefixer = require('autoprefixer')(Array.isArray(options.browsers) ? options.browsers : String(options.browsers).split(/,\s*/));
-        } catch (e) {
-            throw new Error('The \'browsers\' option requires the autoprefixer module to be available. Please install it in the containing project.');
-        }
+        var autoprefixer = loadAndInstantiateAutoprefixer(options.browsers, 'The \'browsers\' option requires the autoprefixer module to be available. Please install it in the containing project.');
+
         // Waiting for https://github.com/segmentio/myth/issues/94
         browsers = autoprefixer.prefixes.browsers;
         browsers.requirements.olderThan = {

commit d3431e3eaa2a8e1bbd5ed54f88a0a7c3531d8fbc

Fixed autoprefixer regression introduced in 7ba12f53.

lib/loadAndInstantiateAutoprefixer.js
@@ -1,8 +1,10 @@
 var semver = require('semver');
 
-module.exports = function loadAndInstantiateAutoprefixer(browsers, errorMessageIfUnavailable) {
-    if (browsers && !Array.isArray(browsers)) {
-        browsers = String(browsers).split(/,\s*/);
+module.exports = function loadAndInstantiateAutoprefixer(browsersOrAutoprefixerInstance, errorMessageIfUnavailable) {
+    if (browsersOrAutoprefixerInstance && browsersOrAutoprefixerInstance.process) {
+        return browsersOrAutoprefixerInstance;
+    } else if (browsersOrAutoprefixerInstance && !Array.isArray(browsersOrAutoprefixerInstance)) {
+        browsersOrAutoprefixerInstance = String(browsersOrAutoprefixerInstance).split(/,\s*/);
     }
 
     var autoprefixer;
@@ -16,9 +18,9 @@ module.exports = function loadAndInstantiateAutoprefixer(browsers, errorMessageI
     }
 
     if (semver.satisfies(require('autoprefixer/package.json').version, '>= 3.0.0')) {
-        autoprefixer = autoprefixer(browsers ? {browsers: browsers} : {});
+        autoprefixer = autoprefixer(browsersOrAutoprefixerInstance ? {browsers: browsersOrAutoprefixerInstance} : {});
     } else {
-        autoprefixer = autoprefixer(browsers);
+        autoprefixer = autoprefixer(browsersOrAutoprefixerInstance);
     }
     return autoprefixer;
 };

lib/transforms/autoprefixer.js
@@ -6,14 +6,13 @@
 
 var loadAndInstantiateAutoprefixer = require('../loadAndInstantiateAutoprefixer');
 
-module.exports = function (options) {
+module.exports = function (autoprefixerOrOptions) {
     return function autoprefixer(assetGraph) {
         var cssAssets = assetGraph.findAssets({type: 'Css'});
 
         if (cssAssets.length > 0) {
             // See https://github.com/ai/autoprefixer#browsers
-            var browsers = typeof options === 'string' ? options.split(/,\s*/) : options,
-                autoprefix = loadAndInstantiateAutoprefixer(browsers, 'autoprefixer transform: Found ' + cssAssets.length + ' css asset(s), but no autoprefixer module is available. Please use npm to install autoprefixer in your project so the autoprefixer transform can require it.');
+            var autoprefix = loadAndInstantiateAutoprefixer(autoprefixerOrOptions, 'autoprefixer transform: Found ' + cssAssets.length + ' css asset(s), but no autoprefixer module is available. Please use npm to install autoprefixer in your project so the autoprefixer transform can require it.');
 
             cssAssets.forEach(function (cssAsset) {
                 cssAsset.text = autoprefix.process(cssAsset.text).css;