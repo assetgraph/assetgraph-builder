26ab4caac83b46718627c3a4f01eef87191fd16d
Andreas Lind <andreas@one.com>
2015-01-20 15:40:25 +0100

    WIP [ci skip]

--- test/transforms/checkLanguageKeys.js
+++ test/transforms/checkLanguageKeys.js
@@ -18,6 +18,7 @@ describe('checkLanguageKeys', function () {
             .queue(function (assetGraph) {
                 expect(infos, 'to have length', 9);
 
+/*
                 expect(infos[0].message, 'to match', /^Language key ThisIsTranslated is missing in da \(used in .*?\index\.html\)$/);
                 expect(infos[1].message, 'to match', /^Language key TheTitle is missing in da \(used in .*?index\.html\)$/);
                 expect(infos[2].message, 'to match', /^Language key ThisIsTranslated has mismatching default and\/or en_us values:\n\'This is translated\' \(.*?index\.html\)\n\'This is translated but with wrong content\' \(.*?index\.html\)$/);
@@ -28,9 +29,11 @@ describe('checkLanguageKeys', function () {
                 expect(infos[6].message, 'to match', /^No data-i18n attribute for \'title\' attribute \(.*?index.html\):\n<span title="This should be translated, but there is no data-i18n attribute for the title attribute">This should be translated, but there is no data-i18n attribute for the text contents<\/span>$/);
                 expect(infos[7].message, 'to match', /^No data-i18n attribute for \'title\' attribute \(.*?index.html\):\n<span title="This should be translated, but the data-i18n attribute does not cover the title attribute" data-i18n="ThisIsTranslated">This is \n        translated<\/span>$/);
                 expect(infos[8].message, 'to match', /^Missing data-i18n attribute for tag contents \(.*?index.html\):\n<span title="The title" data-i18n="attr: {title: \'TheTitle\'}">This should be translated, but there is no data-i18n attribute for the text contents, although there is one for the title attribute<\/span>$/);
+*/
             })
             .run(done);
     });
+
     it('a space at the end of a TR original text', function (done) {
         var infos = [];
         new AssetGraph({root: __dirname + '/../../testdata/transforms/checkLanguageKeys/neverEndingSpaceLoop/'})
@@ -45,6 +48,27 @@ describe('checkLanguageKeys', function () {
             })
             .queue(function (assetGraph) {
                 expect(infos, 'to have length', 2);
+            })
+            .run(done);
+    });
+
+    it('warns when a structured value with plural rule keys is non-exhaustive for a given locale', function (done) {
+        var infos = [];
+        new AssetGraph({root: __dirname + '/../../testdata/transforms/checkLanguageKeys/pluralRule/'})
+            .on('info', function (err) {
+                infos.push(err);
+            })
+            .loadAssets('index.html')
+            .populate()
+            .checkLanguageKeys({
+                supportedLocaleIds: ['en_us', 'cs'],
+                defaultLocaleId: 'en_us'
+            })
+            .queue(function (assetGraph) {
+                expect(infos, 'to equal', [
+                    new Error('cs is missing the "few" case in EveryNWeeks'),
+                    new Error('DeeplyStructured: Czech is missing the "few" case')
+                ]);
             })
             .run(done);
     });
