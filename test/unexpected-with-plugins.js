62a4c12d70f8a5d6dd3503d4ebc07d8c9ea13de9
Andreas Lind <andreas@one.com>
2015-06-09 13:37:01 +0200

    Update unexpected and unexpected-sinon, add unexpected-dom, remove unexpected-jsdom

--- test/unexpected-with-plugins.js
+++ test/unexpected-with-plugins.js
@@ -1,6 +1,7 @@
 var expect = require('unexpected')
     .clone()
     .installPlugin(require('unexpected-sinon'))
+    .installPlugin(require('unexpected-dom'))
     .installPlugin(require('assetgraph/test/unexpectedAssetGraph'));
 
 // expect.output.installPlugin(require('magicpen-prism'));
