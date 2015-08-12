5db5b45b42e5c4381128fcdd0cb0c19b004763b0
Andreas Lind <andreas@one.com>
2015-08-12 10:47:06 +0200

    Switch to assetgraph-teepee.

--- test/unexpected-with-plugins.js
+++ test/unexpected-with-plugins.js
@@ -3,4 +3,4 @@ module.exports = require('unexpected')
     .installPlugin(require('unexpected-sinon'))
     .installPlugin(require('unexpected-dom'))
     .installPlugin(require('unexpected-color'))
-    .installPlugin(require('assetgraph/test/unexpectedAssetGraph'));
+    .installPlugin(require('assetgraph-teepee/test/unexpectedAssetGraph'));
