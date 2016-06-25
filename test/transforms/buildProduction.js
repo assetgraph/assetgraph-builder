b999a1d9daa79eb44e424da4177c4558915098ec
Andreas Lind <andreas@one.com>
2016-06-25 09:31:49 +0200

    buildProduction: Also add crossorigin=... when a CDN is configured using an absolute url.

--- test/transforms/buildProduction.js
+++ test/transforms/buildProduction.js
@@ -591,6 +591,21 @@ describe('buildProduction', function () {
             });
     });
 
+    it('should set crossorigin=anonymous on script and link tags that end up pointing to the configured cdn, also when the cdnRoot is specified as an absolute url', function () {
+        return new AssetGraph({root: __dirname + '/../../testdata/transforms/buildProduction/crossoriginAnonymous/'})
+            .registerRequireJsConfig()
+            .loadAssets('index.html')
+            .buildProduction({
+                version: false,
+                cdnRoot: 'https://cdn.example.com/foo/',
+                inlineByRelationType: {'*': false}
+            })
+            .then(function (assetGraph) {
+                expect(assetGraph.findRelations({type: ['HtmlStyle', 'HtmlScript']}), 'to have length', 2)
+                    .and('to have items satisfying', { node: { attributes: { crossorigin: 'anonymous' } } });
+            });
+    });
+
     it('should handle a test case with a JavaScriptGetStaticUrl relation pointing at a flash file, then running the buildProduction transform with the cdnRoot and cdnFlash options', function (done) {
         new AssetGraph({root: __dirname + '/../../testdata/transforms/buildProduction/GetStaticUrlFlash/'})
             .on('error', done)
