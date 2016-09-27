faa945d86984737cf16c75a68bde1150392dbf1f
Andreas Lind <andreas@one.com>
2016-09-27 08:44:14 +0200

    data-{systemjs => assetgraph}-conditions as it has the potential to be used with other script loaders or even without.

--- test/transforms/cloneForEachConditionValue.js
+++ test/transforms/cloneForEachConditionValue.js
@@ -11,10 +11,10 @@ describe('cloneForEachConditionValue', function () {
             .queue(function (assetGraph) {
                 expect(assetGraph, 'to contain assets', 'Html', 2);
                 expect(assetGraph.findAssets({fileName: 'index.sunny.html'})[0].text, 'to contain', 'sunny')
-                    .and('to contain', '<html data-systemjs-conditions="weather: \'sunny\'">')
+                    .and('to contain', '<html data-assetgraph-conditions="weather: \'sunny\'">')
                     .and('not to contain', 'rainy');
                 expect(assetGraph.findAssets({fileName: 'index.rainy.html'})[0].text, 'to contain', 'rainy')
-                    .and('to contain', '<html data-systemjs-conditions="weather: \'rainy\'">')
+                    .and('to contain', '<html data-assetgraph-conditions="weather: \'rainy\'">')
                     .and('not to contain', 'sunny');
             });
     });
@@ -33,7 +33,7 @@ describe('cloneForEachConditionValue', function () {
 
                 expect(assetGraph.findAssets({fileName: 'index.sunny.happy.html'})[0].text, 'to contain', 'sunny')
                     .and('not to contain', 'rainy').and('to contain', 'happy').and('not to contain', 'sad')
-                    .and('to contain', '<html data-systemjs-conditions="weather: \'sunny\', mood: \'happy\'">');
+                    .and('to contain', '<html data-assetgraph-conditions="weather: \'sunny\', mood: \'happy\'">');
                 expect(assetGraph.findAssets({fileName: 'index.sunny.sad.html'})[0].text, 'to contain', 'sunny')
                     .and('not to contain', 'rainy').and('not to contain', 'happy').and('to contain', 'sad');
                 expect(assetGraph.findAssets({fileName: 'index.rainy.happy.html'})[0].text, 'not to contain', 'sunny')
@@ -44,7 +44,7 @@ describe('cloneForEachConditionValue', function () {
     });
 
     describe('when no explicit condition values are provided', function () {
-        it('should not fan out when there are no relevant data-systemjs-conditions attributes in the HTML', function () {
+        it('should not fan out when there are no relevant data-assetgraph-conditions attributes in the HTML', function () {
             return new AssetGraph({root: __dirname + '/../../testdata/transforms/cloneForEachConditionValue/noConditions/'})
                 .loadAssets('index.html')
                 .populate()
@@ -56,7 +56,7 @@ describe('cloneForEachConditionValue', function () {
     });
 
     describe('when explicit condition values are given', function () {
-        it('should fan out even when there are no relevant data-systemjs-conditions attributes in the HTML', function () {
+        it('should fan out even when there are no relevant data-assetgraph-conditions attributes in the HTML', function () {
             return new AssetGraph({root: __dirname + '/../../testdata/transforms/cloneForEachConditionValue/noConditions/'})
                 .loadAssets('index.html')
                 .populate()
