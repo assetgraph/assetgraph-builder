e97190559f159a263c0a22f8e61994139393f09e
Andreas Lind <andreaslindpetersen@gmail.com>
2018-01-02 20:33:54 +0100

    Fix test

--- test/transforms/buildProduction.js
+++ test/transforms/buildProduction.js
@@ -1607,9 +1607,8 @@ describe('buildProduction', function () {
                 cdnRoot: 'http://cdn.example.com/foo/'
             })
             .queue(function (assetGraph) {
-                // expect(assetGraph.findAssets({url: /\/index\.html$/})[0].text, 'to equal', '<!DOCTYPE html><html><head></head><body><script src=http://cdn.example.com/foo/bundle.9baef1d6a3.js crossorigin=anonymous></script></body></html>');
                 expect(assetGraph.findAssets({url: /\/index\.html$/})[0].text, 'to match', /src=http:\/\/cdn.example.com\/foo\/bundle\.\w+\.js/);
-                expect(assetGraph.findAssets({url: /\/bundle\.\w+\.js$/})[0].text, 'to equal', 'alert(\'foo\')//# sourceMappingURL=http://cdn.example.com/foo/bundle.js.map\n');
+                expect(assetGraph.findAssets({url: /\/bundle\.\w+\.js$/})[0].text, 'to equal', 'alert(\'foo\')//# sourceMappingURL=http://cdn.example.com/foo/bundle.js.256cdc4f81.map\n');
             })
             .run(done);
     });
