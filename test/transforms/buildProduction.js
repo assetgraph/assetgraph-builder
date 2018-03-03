1dc4aedd6dbeb564d1aceec0be15f2b574c99f79
Andreas Lind <andreaslindpetersen@gmail.com>
2018-03-04 00:32:05 +0100

    Fix test

--- test/transforms/buildProduction.js
+++ test/transforms/buildProduction.js
@@ -1432,7 +1432,7 @@ describe('buildProduction', function () {
                 ]
             });
 
-            expect(assetGraph.findAssets(), 'to satisfy', [
+            expect(assetGraph.findAssets({isLoaded: true}), 'to satisfy', [
                 {
                     type: 'Html',
                     fileName: 'index.html'
