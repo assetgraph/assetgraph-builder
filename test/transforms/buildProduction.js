assetgraph/assetgraph-builder

commit 879b4a11d7238b3ebc0bb5005672301b1b66d011

Fix moveAssetsInOrder transform: file:///private/tmp/static/font.eot already exists in the graph, cannot update url
Reported by @salomvary on gitter

lib/transforms/buildProduction.js
@@ -396,13 +396,13 @@ module.exports = function (options) {
 
         if (!options.noFileRev) {
             await assetGraph.moveAssetsInOrder(moveAssetsInOrderQuery, (asset, assetGraph) => {
-                let targetUrl = '/static/';
+                let baseUrl = (assetGraph.canonicalRoot || assetGraph.root) + 'static/';
                 // Conservatively assume that all JavaScriptStaticUrl relations pointing at non-images are intended to be fetched via XHR
                 // and thus cannot be put on a CDN because of same origin restrictions:
                 const hasIncomingJavaScriptStaticUrlOrServiceWorkerRelations = assetGraph.findRelations({to: asset, type: {$in: ['JavaScriptStaticUrl', 'JavaScriptServiceWorkerRegistration', 'HtmlServiceWorkerRegistration']}}).length > 0;
                 if (options.cdnRoot && asset.type !== 'Htc' && asset.extension !== '.jar' && (asset.type !== 'Html' || options.cdnHtml) && (asset.isImage || !hasIncomingJavaScriptStaticUrlOrServiceWorkerRelations) || (options.cdnRoot && options.cdnFlash && asset.type === 'Flash')) {
-                    targetUrl = options.cdnRoot;
-                    assetGraph.findRelations({to: asset}).forEach(function (incomingRelation) {
+                    baseUrl = options.cdnRoot;
+                    assetGraph.findRelations({ to: asset }).forEach(function (incomingRelation) {
                         if (/^\/\//.test(options.cdnRoot)) {
                             incomingRelation.hrefType = 'protocolRelative';
                         } else if ((asset.type === 'SourceMap' || hasIncomingJavaScriptStaticUrlOrServiceWorkerRelations) && /^https?:/.test(options.cdnRoot)) {
@@ -418,7 +418,15 @@ module.exports = function (options) {
                         }
                     });
                 }
-                return targetUrl + asset.fileName;
+                // Find a way to avoid repeating this construct over and over.
+                // Maybe the _urlIndex thing is too strict.
+                let targetUrl = baseUrl + asset.fileName;
+                let nextSuffixToTry = 1;
+                while (assetGraph._urlIndex[targetUrl]) {
+                    targetUrl = `${baseUrl}${asset.baseName}-${nextSuffixToTry}${asset.extension}`;
+                    nextSuffixToTry += 1;
+                }
+                return targetUrl;
             });
             await assetGraph.moveAssetsInOrder(moveAssetsInOrderQuery, function (asset, assetGraph) {
                 return asset.fileName.replace(/\.[^.]+$/, '') + '.' + asset.md5Hex.substr(0, 10) + asset.extension + asset.url.replace(/^[^#\?]*(?:)/, ''); // Preserve query string and fragment identifier

test/transforms/buildProduction.js
@@ -1566,4 +1566,15 @@ describe('buildProduction', function () {
                 .and('not to contain', '.toString(\'url\')');
         });
     });
+
+    // Turning on deduplication for fonts via mergeIdenticalAssets would require changes here:
+    it('should handle a case with an ?#iefix hack', async function () {
+        const assetGraph = new AssetGraph({root: __dirname + '/../../testdata/transforms/buildProduction/fontWithIeFix/'});
+        await assetGraph.loadAssets('index.html');
+        await assetGraph.buildProduction();
+
+        expect(assetGraph, 'to contain assets', 4);
+        expect(assetGraph, 'to contain asset', {path: '/static/', fileName: 'font.0c064252fc.eot'})
+            .and('to contain asset', {path: '/static/', fileName: 'font.0c064252fc.eot'});
+    });
 });

testdata/transforms/buildProduction/fontWithIeFix/index.html
@@ -1,0 +1,22 @@
+<!doctype html>
+
+<head>
+    <style>
+        @font-face {
+            font-family: 'font';
+            src: url('font.eot');
+            font-weight: 100;
+            font-style: normal;
+        }
+
+        @font-face {
+            font-family: 'font-thin';
+            src: url('font.eot');
+            src: local('font thin'), local('font-thin'), url('font.eot?#iefix');
+        }
+    </style>
+
+</head>
+
+<body>
+</body>

commit 1f49f387583056abf8437f95956d9193871356fc

Don't move files to the canonical root.
Fixes bug introduced in 879b4a11d

https://github.com/mochajs/mocha/pull/3412#issuecomment-396038075

lib/transforms/buildProduction.js
@@ -589,8 +589,7 @@ module.exports = function(options) {
       await assetGraph.moveAssetsInOrder(
         moveAssetsInOrderQuery,
         (asset, assetGraph) => {
-          let baseUrl =
-            (assetGraph.canonicalRoot || assetGraph.root) + 'static/';
+          let baseUrl = assetGraph.root + 'static/';
           // Conservatively assume that all JavaScriptStaticUrl relations pointing at non-images are intended to be fetched via XHR
           // and thus cannot be put on a CDN because of same origin restrictions:
           const hasIncomingJavaScriptStaticUrlOrServiceWorkerRelations =

test/transforms/buildProduction.js
@@ -1014,49 +1014,73 @@ describe('buildProduction', function() {
     );
   });
 
-  it('should handle a test case with an RSS feed (#118)', async function() {
-    const assetGraph = new AssetGraph({
-      root: __dirname + '/../../testdata/transforms/buildProduction/rss/',
-      canonicalRoot: 'http://www.someexamplerssdomain.com/'
+  describe('with the canonicalRoot option', function() {
+    it('should handle a test case with an RSS feed (#118)', async function() {
+      const assetGraph = new AssetGraph({
+        root: __dirname + '/../../testdata/transforms/buildProduction/rss/',
+        canonicalRoot: 'http://www.someexamplerssdomain.com/'
+      });
+      await assetGraph.loadAssets('index.html');
+      await assetGraph.populate();
+
+      expect(assetGraph, 'to contain asset', {
+        contentType: 'application/rss+xml',
+        type: 'Rss'
+      });
+
+      await assetGraph.buildProduction({
+        version: false
+      });
+
+      expect(assetGraph.findRelations(), 'to satisfy', [
+        {
+          type: 'HtmlAlternateLink',
+          href: 'rssFeed.xml',
+          canonical: false
+        },
+        {
+          type: 'RssChannelLink',
+          href: 'index.html',
+          canonical: false
+        },
+        {
+          type: 'XmlHtmlInlineFragment'
+        },
+        {
+          type: 'HtmlImage',
+          canonical: true,
+          href: 'http://www.someexamplerssdomain.com/static/foo.d65dd5318f.png'
+        }
+      ]);
+
+      expect(
+        assetGraph.findAssets({ type: 'Rss' })[0].text,
+        'to equal',
+        '<?xml version="1.0" encoding="UTF-8"?>\n<rss version="2.0">\n<channel>\n <title>RSS Title</title>\n <description>This is an example of an RSS feed</description>\n <link>index.html</link>\n <lastBuildDate>Mon, 06 Sep 2010 00:01:00 +0000 </lastBuildDate>\n <pubDate>Mon, 06 Sep 2009 16:20:00 +0000 </pubDate>\n <ttl>1800</ttl>\n <item>\n  <title>Example entry</title>\n  <description>Here is some text containing an interesting description and an image: &lt;img src=http://www.someexamplerssdomain.com/static/foo.d65dd5318f.png>.</description>\n  <link>http://www.wikipedia.org/</link>\n  <guid>unique string per item</guid>\n  <pubDate>Mon, 06 Sep 2009 16:20:00 +0000 </pubDate>\n </item>\n</channel>\n</rss>'
+      );
     });
-    await assetGraph.loadAssets('index.html');
-    await assetGraph.populate();
 
-    expect(assetGraph, 'to contain asset', {
-      contentType: 'application/rss+xml',
-      type: 'Rss'
+    // https://github.com/mochajs/mocha/pull/3412#issuecomment-396038075
+    it('should keep static files within assetGraph.root', async function() {
+      const assetGraph = new AssetGraph({
+        root:
+          __dirname +
+          '/../../testdata/transforms/buildProduction/canonicalRoot/',
+        canonicalRoot: 'http://www.example.com/'
+      });
+      await assetGraph.loadAssets('index.html');
+      await assetGraph.populate();
+
+      await assetGraph.buildProduction({
+        version: false,
+        inlineByRelationType: {}
+      });
+
+      expect(assetGraph, 'to contain asset', {
+        type: 'JavaScript',
+        url: `${assetGraph.root}static/script.7e514b97a9.js`
+      });
     });
-
-    await assetGraph.buildProduction({
-      version: false
-    });
-
-    expect(assetGraph.findRelations(), 'to satisfy', [
-      {
-        type: 'HtmlAlternateLink',
-        href: 'rssFeed.xml',
-        canonical: false
-      },
-      {
-        type: 'RssChannelLink',
-        href: 'index.html',
-        canonical: false
-      },
-      {
-        type: 'XmlHtmlInlineFragment'
-      },
-      {
-        type: 'HtmlImage',
-        canonical: true,
-        href: 'http://www.someexamplerssdomain.com/static/foo.d65dd5318f.png'
-      }
-    ]);
-
-    expect(
-      assetGraph.findAssets({ type: 'Rss' })[0].text,
-      'to equal',
-      '<?xml version="1.0" encoding="UTF-8"?>\n<rss version="2.0">\n<channel>\n <title>RSS Title</title>\n <description>This is an example of an RSS feed</description>\n <link>index.html</link>\n <lastBuildDate>Mon, 06 Sep 2010 00:01:00 +0000 </lastBuildDate>\n <pubDate>Mon, 06 Sep 2009 16:20:00 +0000 </pubDate>\n <ttl>1800</ttl>\n <item>\n  <title>Example entry</title>\n  <description>Here is some text containing an interesting description and an image: &lt;img src=http://www.someexamplerssdomain.com/static/foo.d65dd5318f.png>.</description>\n  <link>http://www.wikipedia.org/</link>\n  <guid>unique string per item</guid>\n  <pubDate>Mon, 06 Sep 2009 16:20:00 +0000 </pubDate>\n </item>\n</channel>\n</rss>'
-    );
   });
 
   it('should keep identical inline styles in svg files inlined', async function() {

testdata/transforms/buildProduction/canonicalRoot/index.html
@@ -1,0 +1,7 @@
+<!DOCTYPE html>
+<html>
+<head></head>
+<body>
+  <script src="script.js"></script>
+</body>
+</html>

testdata/transforms/buildProduction/canonicalRoot/script.js
@@ -1,0 +1,1 @@
+alert('script');